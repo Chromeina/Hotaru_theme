2bd331e35274292b928ebad0ef93097ac489851e
Chromeina <58142866+Chromeina@users.noreply.github.com>
2021-08-18 23:34:45 +0800

    Update UpdateTime.vue

--- src/components/UpdateTime.vue
+++ src/components/UpdateTime.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="updated">最后更新: {{ timeSince }}</div>
+  <div class="updated">最後更新: {{ timeSince }}</div>
 </template>
 
 <script lang="ts">
@@ -14,7 +14,7 @@ export default defineComponent({
     const timeSince = computed((): string => {
       const nowTime: number = Date.now() / 1000;
       if (!props.updated)
-        return '从未.';
+        return '從未.';
       const seconds: number = Math.floor(nowTime - props.updated);
       let interval = Math.floor(seconds / 31536000);
       if (interval > 1)
@@ -27,14 +27,14 @@ export default defineComponent({
         return interval + ' 日前.';
       interval = Math.floor(seconds / 3600);
       if (interval > 1)
-        return interval + ' 小时前.';
+        return interval + ' 小時前.';
       interval = Math.floor(seconds / 60);
       if (interval > 1)
-        return interval + ' 分钟前.';
+        return interval + ' 分鐘前.';
       /*if(Math.floor(seconds) >= 5)
           return Math.floor(seconds) + " seconds";*/
       else
-        return '几秒前.';
+        return '幾秒鐘前.';
     });
     return {
       timeSince
