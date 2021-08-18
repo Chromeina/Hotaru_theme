84ed543163e8f2938015a09224d63350ddba85a7
Chromeina <58142866+Chromeina@users.noreply.github.com>
2021-08-18 23:33:01 +0800

    Update TheError.vue

--- src/components/TheError.vue
+++ src/components/TheError.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="error">
-    若長時間出現此消息，請確保您已啟用Javascript！<br>否則雲監控主服務（服務端）未啟動或已關閉。
+    若長時間出現此訊息，請確保你已經啟用Javascript！<br>又或者線上監控主服務（伺服器端）尚未啟動或已關閉。
   </p>
 </template>
 <script lang="ts">
